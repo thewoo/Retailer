7f2e4ef701ba0ef5a26a28f14ab58e1029064dfe
Jorge Pardo <jorgepardo@Caduceus.local>
2012-10-01 16:06:41 +0200

    Initial Commit

---
+++ Retailler/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Retailler
+//
+//  Created by Jorge Pardo on 01/10/12.
+//  Copyright (c) 2012 Jorge Pardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Retailler/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Retailler
+//
+//  Created by Jorge Pardo on 01/10/12.
+//  Copyright (c) 2012 Jorge Pardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
